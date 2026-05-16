cd7e920d3fcf81a52003c2e56661c47286833389
Hareem Adderley <hadderley@kingpinapps.com>
2026-05-15 19:41:55 -0500

    fix: use latest uplc

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/Kingpin-Apps/swift-cardano-core.git", from: "0.3.21"),
         .package(url: "https://github.com/Kingpin-Apps/swift-cardano-chain.git", from: "0.4.2"),
-        .package(url: "https://github.com/Kingpin-Apps/swift-cardano-uplc.git", from: "0.1.6"),
+        .package(url: "https://github.com/Kingpin-Apps/swift-cardano-uplc.git", from: "0.1.7"),
         .package(url: "https://github.com/Kingpin-Apps/swift-ncal.git", from: "0.2.3"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.12.0"),
     ],
